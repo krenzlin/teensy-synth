e94e286e5b93e8ddadf1420785e085afa768a186
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2021-01-09 15:07:32 +0100

    Simplify audio_loop for now

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,12 @@ IntervalTimer timer;
 uint16_t m_to_incr[127];
 
 void audio_loop() {
-    static uint16_t phase_1{0};
-	static uint16_t phase_2{0};
+    static uint16_t phase{0};
 
-    dac.write((phase_1 + phase_2) >> 1);
+    dac.write(phase);
 
-    phase_1 += m_to_incr[69];
-    phase_2 += m_to_incr[59];
-
-    phase_1 = misc::wrap(phase_1);
-    phase_2 = misc::wrap(phase_2);
+    phase += m_to_incr[69];
+    phase = misc::wrap(phase);
 }
 
 
