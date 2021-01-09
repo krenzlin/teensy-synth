cfee149febbe25316cddceefe122d9067eacd4f1
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2021-01-09 13:57:30 +0100

    Remove unnecessary misc::f_to_incr as it does not help making things clear

--- src/misc.cpp
+++ src/misc.cpp
@@ -6,12 +6,8 @@ float misc::m_to_f(const uint8_t note, const float tuning) {
     return pow(2.0, (note - 69.0)/12.0) * tuning;
 }
 
-uint16_t misc::f_to_incr(const float frequency, const uint16_t sr, const uint16_t max) {
-    return (uint16_t) frequency / (float) sr * max;
-}
-
 uint16_t misc::m_to_incr(const uint8_t note) {
-    return misc::f_to_incr(misc::m_to_f(note));
+    return misc::m_to_f(note) / (float) misc::samplerate * misc::max_dac_value;
 }
 
 uint16_t misc::sr_to_us(const uint16_t sr) {
--- src/misc.h
+++ src/misc.h
@@ -8,7 +8,6 @@ namespace misc {
     constexpr uint16_t max_dac_value = (1 << 12);
 
     float m_to_f(const uint8_t note, const float tuning=tuning);
-    uint16_t f_to_incr(const float f, const uint16_t sr=samplerate, const uint16_t max=max_dac_value);
     uint16_t m_to_incr(const uint8_t note);
 
     uint16_t sr_to_us(const uint16_t sr=samplerate);
