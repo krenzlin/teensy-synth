f55ddb9d86e664acf7a79a9c7bd685c6cc107b87
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2021-01-10 13:13:29 +0100

    Add Saw class w/ note_on; audio_loop just calls process()

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,33 @@ hal::DAC dac;
 IntervalTimer timer;
 uint32_t m_to_incr[127];
 
+
+class Saw {
+    uint32_t phase{0};
+    uint32_t p_incr {0};
+    int32_t sample{0};
+
+    public:
+
+        void note_on(uint8_t note, uint8_t /*velocity*/) {
+            p_incr = m_to_incr[note];
+        }
+
+        int32_t process() {
+            sample = misc::u32_to_s32(phase);
+
+            phase += p_incr; // no need to wrap phase as uint32 overflow does that for us
+
+            return sample;
+        }
+};
+
+
+Saw saw{};
+
+
 void audio_loop() {
-    static uint32_t phase{0};
-
-    dac.write(misc::u32_to_s32(phase));
-
-    phase += m_to_incr[69];
+    dac.write(saw.process());
 }
 
 
@@ -21,6 +42,8 @@ extern "C" int main(void) {
     for (auto i=0; i<127; i++) {
         m_to_incr[i] = misc::m_to_incr(i);
     }
+
+    saw.note_on(72, 127);
 
     timer.begin(audio_loop, misc::sr_to_us());
 }
