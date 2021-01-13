66488d0778dbbf0c97662825a64aea090b58935a
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2021-01-13 12:28:38 +0100

    Play two notes a 4th apart

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,25 @@
 hal::DAC dac;
 IntervalTimer timer;
 osc::Saw saw{};
+osc::Saw saw2{};
 
 
 void audio_loop() {
-    dac.write(saw.process());
+    dac.write((saw.process() >> 1) + (saw2.process() >> 1));
 }
-
 
 extern "C" int main(void) {
     saw.note_on(69, 127);
+    saw2.note_on(69+5, 127);
+
 
     timer.begin(audio_loop, misc::sr_to_us());
 
+    static uint8_t note {69};
     while(1) {
         delay(1000);
-        saw.note_on(misc::random_note(50, 80));
+        note = misc::random_note(50, 80);
+        saw.note_on(note);
+        saw2.note_on(note+5);
     }
 }
