05dfd768d1a657310313942eed0f1dbc9722d034
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2021-01-23 00:40:35 +0100

    Write new Makefile that compiles both project src and libs aswell as teensy core and arduino libs; add example main using Audio

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,40 @@
-#include "WProgram.h"
-#include "IntervalTimer.h"
-#include <stdint.h>
-#include "dac.h"
-#include "misc.h"
-#include "osc.h"
+// Advanced Microcontroller-based Audio Workshop
+//
+// http://www.pjrc.com/store/audio_tutorial_kit.html
+// https://hackaday.io/project/8292-microcontroller-audio-workshop-had-supercon-2015
+//
+// Part 1-2: Test Hardware
+//
+// Simple beeping is pre-loaded on the Teensy, so
+// it will create sound and print info to the serial
+// monitor when plugged into a PC.
+//
+// This program is supposed to be pre-loaded before
+// the workshop, so Teensy+Audio will beep when
+// plugged in.
+
+#include <Audio.h>
+
+AudioSynthWaveform    waveform1;
+AudioOutputI2S        i2s1;
+AudioConnection       patchCord1(waveform1, 0, i2s1, 0);
+AudioConnection       patchCord2(waveform1, 0, i2s1, 1);
+AudioControlSGTL5000  sgtl5000_1;
 
 
-hal::DAC dac;
-IntervalTimer timer;
-osc::Saw saw{};
-osc::Saw saw2{};
+int main() {
+    AudioMemory(10);
+    sgtl5000_1.enable();
+    sgtl5000_1.volume(0.3);
+    waveform1.begin(WAVEFORM_SINE);
 
-
-uint32_t max_us {0};
-uint32_t alltime_max_us {0};
-
-void audio_loop() {
-    uint32_t start = micros();
-
-    dac.write((saw.process() >> 1) + (saw2.process() >> 1));
-
-    uint32_t us = micros() - start;
-    if (us > max_us) max_us = us;
-    if (us > alltime_max_us) alltime_max_us = us;
-}
-
-extern "C" int main(void) {
-    while (!Serial); // wait for serial monitor
-    Serial.begin(9600);
-
-    saw.note_on(69, 127);
-    saw2.note_on(69+5, 127);
-
-
-    timer.begin(audio_loop, misc::sr_to_us());
+    delay(1000);
 
     while(1) {
-        delay(1000);
-        uint8_t note = misc::random_note(50, 80);
-        saw.note_on(note);
-        saw2.note_on(note+5);
-        Serial.println("----");
-        Serial.print("us: ");
-        Serial.print(max_us);
-        Serial.print(" | ");
-        Serial.println(alltime_max_us);
-        max_us = 0;
-
+        waveform1.frequency(440);
+        waveform1.amplitude(0.9);
+        delay(250);
+        waveform1.amplitude(0);
+        delay(1750);
     }
 }
--- teensy4.mk
+++ teensy4.mk
@@ -1,122 +1,86 @@
-# Teensyduino Core Library
-# http://www.pjrc.com/teensy/
-# Copyright (c) 2019 PJRC.COM, LLC.
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# 1. The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# 2. If the Software is incorporated into a build system that allows
-# selection among a list of target devices, then similar target
-# devices manufactured by PJRC.COM must be included in the list of
-# target devices and selectable in the same manner.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+TARGET = bin/synth
 
-# set your MCU type here, or make command line `make MCU=IMXRT1062`
-MCU=IMXRT1062
+SRCDIR = src
+BUILDDIR = build
+LOCALLIBDIR = libs
+ARDUINOPATH = arduino-1.8.13
+ARDUINOLIBS = SD SPI SerialFlash Wire
 
-# The name of your project (used to name the compiled .hex file)
-TARGET = synth
+MCU = IMXRT1062
 
-# configurable options
-OPTIONS = -DF_CPU=600000000 -DUSB_SERIAL -DLAYOUT_US_ENGLISH -DUSING_MAKEFILE
+# derived paths
+COREDIR = $(ARDUINOPATH)/hardware/teensy/avr/cores/teensy4
+MCU_LD = $(COREDIR)/imxrt1062.ld
+ARDUINOLIBDIR = $(ARDUINOPATH)/hardware/teensy/avr/libraries
+TOOLSPATH = $(ARDUINOPATH)/hardware/tools
 
-# options needed by many Arduino libraries to configure for Teensy 4.0
-OPTIONS += -D__$(MCU)__ -DARDUINO=10810 -DTEENSYDUINO=149 -DARDUINO_TEENSY40
+## get .c/.cpp files and includes for ...
+# source dir
+INC := -I$(SRCDIR)
+C_FILES := $(wildcard $(SRCDIR)/*.c)
+CPP_FILES := $(wildcard $(SRCDIR)/*.cpp)
+S_FILES := $(wildcard $(SRCDIR)/*.S)
 
-# for Cortex M7 with single & double precision FPU
-CPUOPTIONS = -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-d16 -mthumb
+# core library
+INC += -I$(COREDIR)
+C_FILES += $(wildcard $(COREDIR)/*.c)
+CPP_FILES += $(wildcard $(COREDIR)/*.cpp)
+CPP_FILES := $(filter-out $(COREDIR)/main.cpp, $(CPP_FILES)) # remove main.cpp from core
+S_FILES += $(wildcard $(COREDIR)/*.S)
 
-# use this for a smaller, no-float printf
-#SPECS = --specs=nano.specs
+# local libraries
+INC += $(foreach lib,$(filter %/, $(wildcard $(LOCALLIBDIR)/*/)), -I$(lib))
+INC += $(foreach lib,$(filter %/, $(wildcard $(LOCALLIBDIR)/*/utility/)), -I$(lib))
 
-# Other Makefiles and project templates for Teensy
-#
-# https://forum.pjrc.com/threads/57251?p=213332&viewfull=1#post213332
-# https://github.com/apmorton/teensy-template
-# https://github.com/xxxajk/Arduino_Makefile_master
-# https://github.com/JonHylands/uCee
+C_FILES += $(wildcard $(LOCALLIBDIR)/*/*.c)
+C_FILES += $(wildcard $(LOCALLIBDIR)/*/utility/*.c)
 
+CPP_FILES += $(wildcard $(LOCALLIBDIR)/*/*.cpp)
+CPP_FILES += $(wildcard $(LOCALLIBDIR)/*/utility/*.cpp)
 
-#************************************************************************
-# Location of Teensyduino utilities, Toolchain, and Arduino Libraries.
-# To use this makefile without Arduino, copy the resources from these
-# locations and edit the pathnames.  The rest of Arduino is not needed.
-#************************************************************************
+S_FILES += $(wildcard $(LOCALLIBDIR)/*/*.S)
+S_FILES += $(wildcard $(LOCALLIBDIR)/*/utility/*.S)
 
-ARDUINOPATH=arduino-1.8.13
+# arduino libraries
+ARDUINOLIBDIRS = $(foreach lib, $(ARDUINOLIBS), $(ARDUINOLIBDIR)/$(lib))
+INC += $(foreach lib,$(ARDUINOLIBDIRS), -I$(lib))
+INC += $(foreach lib,$(ARDUINOLIBDIRS), -I$(lib)/utility)
 
-# path location for Teensy Loader, teensy_post_compile and teensy_reboot (on Linux)
-TOOLSPATH = $(abspath $(ARDUINOPATH)/hardware/tools)
+C_FILES += $(foreach lib,$(ARDUINOLIBDIRS), $(wildcard $(lib)/*.c))
+C_FILES += $(foreach lib,$(ARDUINOLIBDIRS), $(wildcard $(lib)/utility/*.c))
 
-# path location for Arduino libraries
-LIBRARYPATH = libraries
+CPP_FILES += $(foreach lib,$(ARDUINOLIBDIRS), $(wildcard $(lib)/*.cpp))
+CPP_FILES += $(foreach lib,$(ARDUINOLIBDIRS), $(wildcard $(lib)/utility/*.cpp))
 
+S_FILES += $(foreach lib,$(ARDUINOLIBDIRS), $(wildcard $(lib)/*.S))
+S_FILES += $(foreach lib,$(ARDUINOLIBDIRS), $(wildcard $(lib)/utility/*.S))
 
-# path location for the arm-none-eabi compiler
-COMPILERPATH = $(abspath $(ARDUINOPATH)/hardware/tools/arm/bin)
-
-COREPATH = $(abspath $(ARDUINOPATH)/hardware/teensy/avr/cores/teensy4)
-SRCPATH = src
-# directory to build in
-BUILDDIR = $(abspath $(CURDIR)/build)
-
-#************************************************************************
-# Settings below this point usually do not need to be edited
-#************************************************************************
-
-# CPPFLAGS = compiler options for C and C++
-CPPFLAGS = -Wall -g -O2 $(CPUOPTIONS) -MMD $(OPTIONS) -I$(SRCPATH) -I$(COREPATH) -ffunction-sections -fdata-sections
-
-# compiler options for C++ only
-CXXFLAGS = -std=gnu++14 -felide-constructors -fno-exceptions -fpermissive -fno-rtti -Wno-error=narrowing
-
-# compiler options for C only
-CFLAGS =
-
-# linker options
-LDFLAGS = -Os -Wl,--gc-sections,--relax $(SPECS) $(CPUOPTIONS) -T$(MCU_LD)
-
-# additional libraries to link
-LIBS = -larm_cortexM7lfsp_math -lm -lstdc++
-
+SOURCES := $(C_FILES:.c=.o) $(CPP_FILES:.cpp=.o) $(S_FILES:.S=.o)
+OBJS := $(foreach src,$(SOURCES), $(BUILDDIR)/$(src))
 
 # names for the compiler programs
+COMPILERPATH = $(ARDUINOPATH)/hardware/tools/arm/bin
 CC = $(COMPILERPATH)/arm-none-eabi-gcc
 CXX = $(COMPILERPATH)/arm-none-eabi-g++
 OBJCOPY = $(COMPILERPATH)/arm-none-eabi-objcopy
 SIZE = $(COMPILERPATH)/arm-none-eabi-size
 
-# automatically create lists of the sources and objects
-LC_FILES := $(wildcard $(LIBRARYPATH)/*/*.c)
-LCPP_FILES := $(wildcard $(LIBRARYPATH)/*/*.cpp)
-TC_FILES := $(wildcard $(COREPATH)/*.c)
-TCPP_FILES := $(wildcard $(COREPATH)/*.cpp)
-TCPP_FILES := $(filter-out $(COREPATH)/main.cpp, $(TCPP_FILES))
-C_FILES := $(wildcard $(SRCPATH)/*.c)
-CPP_FILES := $(wildcard $(SRCPATH)/*.cpp)
-INO_FILES := $(wildcard $(SRCPATH)/*.ino)
-
-# include paths for libraries
-L_INC := $(foreach lib,$(filter %/, $(wildcard $(LIBRARYPATH)/*/)), -I$(lib))
-
-SOURCES := $(C_FILES:.c=.o) $(CPP_FILES:.cpp=.o) $(INO_FILES:.ino=.o) $(TC_FILES:.c=.o) $(TCPP_FILES:.cpp=.o) $(LC_FILES:.c=.o) $(LCPP_FILES:.cpp=.o)
-OBJS := $(foreach src,$(SOURCES), $(BUILDDIR)/$(src))
+# configurable options
+OPTIONS = -DF_CPU=600000000 -DUSB_SERIAL -DLAYOUT_US_ENGLISH -DUSING_MAKEFILE
+# options needed by many Arduino libraries to configure for Teensy 4.0
+OPTIONS += -D__$(MCU)__ -DARDUINO=10810 -DTEENSYDUINO=149 -DARDUINO_TEENSY40
+# for Cortex M7 with single & double precision FPU
+CPUOPTIONS = -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-d16 -mthumb
+# CPPFLAGS = compiler options for C and C++
+CPPFLAGS = -Wall -g -O2 $(CPUOPTIONS) -MMD $(OPTIONS) -ffunction-sections -fdata-sections
+# compiler options for C++ only
+CXXFLAGS = -std=gnu++14 -felide-constructors -fno-exceptions -fpermissive -fno-rtti -Wno-error=narrowing
+# compiler options for C only
+CFLAGS =
+# linker options
+LDFLAGS = -Os -Wl,--gc-sections,--relax $(SPECS) $(CPUOPTIONS) -T$(MCU_LD)
+# additional libraries to link
+LIBS = -larm_cortexM7lfsp_math -lm -lstdc++
 
 all: hex
 
@@ -135,17 +99,17 @@ upload: post_compile reboot
 $(BUILDDIR)/%.o: %.c
 	@echo -e "[CC]\t$<"
 	@mkdir -p "$(dir $@)"
-	@$(CC) $(CPPFLAGS) $(CFLAGS) $(L_INC) -o "$@" -c "$<"
+	@$(CC) $(CPPFLAGS) $(CFLAGS) $(INC) -o "$@" -c "$<"
 
 $(BUILDDIR)/%.o: %.cpp
 	@echo -e "[CXX]\t$<"
 	@mkdir -p "$(dir $@)"
-	@$(CXX) $(CPPFLAGS) $(CXXFLAGS) $(L_INC) -o "$@" -c "$<"
+	@$(CXX) $(CPPFLAGS) $(CXXFLAGS) $(INC) -o "$@" -c "$<"
 
-$(BUILDDIR)/%.o: %.ino
-	@echo -e "[CXX]\t$<"
+$(BUILDDIR)/%.o: %.S
+	@echo -e "[S]\t$<"
 	@mkdir -p "$(dir $@)"
-	@$(CXX) $(CPPFLAGS) $(CXXFLAGS) $(L_INC) -o "$@" -x c++ -include Arduino.h -c "$<"
+	@$(CXX) $(CPPFLAGS) $(CXXFLAGS) $(INC) -o "$@" -c "$<"
 
 $(TARGET).elf: $(OBJS) $(MCU_LD)
 	@echo -e "[LD]\t$@"
@@ -158,10 +122,6 @@ $(TARGET).elf: $(OBJS) $(MCU_LD)
 
 # compiler generated dependency info
 -include $(OBJS:.o=.d)
-
-# make "MCU" lower case
-LOWER_MCU := $(subst A,a,$(subst B,b,$(subst C,c,$(subst D,d,$(subst E,e,$(subst F,f,$(subst G,g,$(subst H,h,$(subst I,i,$(subst J,j,$(subst K,k,$(subst L,l,$(subst M,m,$(subst N,n,$(subst O,o,$(subst P,p,$(subst Q,q,$(subst R,r,$(subst S,s,$(subst T,t,$(subst U,u,$(subst V,v,$(subst W,w,$(subst X,x,$(subst Y,y,$(subst Z,z,$(MCU)))))))))))))))))))))))))))
-MCU_LD = $(COREPATH)/$(LOWER_MCU).ld
 
 clean:
 	@echo Cleaning...
