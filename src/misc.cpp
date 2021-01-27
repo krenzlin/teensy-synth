f61c4a9e633c84279a65998477f971fd8634a6ff
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2021-01-27 11:13:05 +0100

    Specify some types

--- src/misc.cpp
+++ src/misc.cpp
@@ -3,7 +3,7 @@
 
 
 float misc::m_to_f(const uint8_t note, const float tuning) {
-    return pow(2.0, (note - 69.0)/12.0) * tuning;
+    return pow(2.f, (note - 69.f)/12.f) * tuning;
 }
 
 uint32_t misc::m_to_incr(const uint8_t note) {
@@ -11,7 +11,7 @@ uint32_t misc::m_to_incr(const uint8_t note) {
 }
 
 uint16_t misc::sr_to_us(const uint16_t sr) {
-    return 1000000 / sr;
+    return 1000000U / sr;
 }
 
 /* Fast random implementation
@@ -29,8 +29,8 @@ important: random seed must not be zero
 */
 uint32_t misc::fast_rand() {
     static uint32_t X_k {misc::random_seed};
-    const uint32_t a {48271};
-    const uint32_t m {2147483647};
+    const uint32_t a {48271U};
+    const uint32_t m {2147483647U};
 
     X_k = (a * X_k) % m;
     return X_k;
@@ -39,14 +39,14 @@ uint32_t misc::fast_rand() {
 // range [0..1.0)
 float misc::fast_float_rand() {
     static uint32_t X_k {misc::random_seed};
-    constexpr uint32_t a = 48271;
-    constexpr float m_1 = 4.6566129e-010f; // 1/(2^31-1) = 1/2147483647
+    constexpr uint32_t a {48271U};
+    constexpr float m_1 {4.6566129e-010f}; // 1/(2^31-1) = 1/2147483647
 
     X_k *=  a;
     return (float) X_k * m_1 / 2.f;
 }
 
 uint8_t misc::random_note(uint8_t min, uint8_t max) {
-    uint8_t range {max - min + 1};
+    uint8_t range = max - min + 1;
     return min + misc::fast_float_rand() * range;
 }
